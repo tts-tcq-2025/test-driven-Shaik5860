f89e728f964201e40c845d9a621977f0d9a017f0
Shaik5860 <waseemahammad.shaik@gmail.com>
2025-10-15 09:16:17 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 void run_all_tests();
 
 int main() {
-    std::cout << "Running StringCalculator tests...\n";
+    std::cout << "Running StringCalculator tests\n";
     run_all_tests();
     return 0;
 }
