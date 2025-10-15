5d9e43551a0c74004ea04cba86c447df0816af9e
Shaik5860 <waseemahammad.shaik@gmail.com>
2025-10-15 09:02:43 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+// Declaration from test file
+void run_all_tests();
+
+int main() {
+    std::cout << "Running StringCalculator tests...\n";
+    run_all_tests();
+    return 0;
+}
